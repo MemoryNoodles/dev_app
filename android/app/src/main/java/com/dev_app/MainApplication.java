64a8c6c93c70271c7c02b3d03f87a7de604c79bb
MemoryNoodles <729804002@qq.com>
2020-05-05 19:48:12 +0800

    一半的包

--- android/app/src/main/java/com/dev_app/MainApplication.java
+++ android/app/src/main/java/com/dev_app/MainApplication.java
@@ -3,6 +3,29 @@ package com.dev_app;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.RNFetchBlob.RNFetchBlobPackage;
+import fr.greweb.reactnativeviewshot.RNViewShotPackage;
+import com.brentvatne.react.ReactVideoPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.ninty.system.setting.SystemSettingPackage;
+import com.futurepress.staticserver.FPStaticServerPackage;
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
+import com.beefe.picker.PickerViewPackage;
+import com.github.yamill.orientation.OrientationPackage;
+import cn.jystudio.local.barcode.recognizer.LocalBarcodeRecognizerPackage;
+import com.BV.LinearGradient.LinearGradientPackage;
+import com.imagepicker.ImagePickerPackage;
+import com.reactnative.ivpusic.imagepicker.PickerPackage;
+import com.marcshilling.idletimer.IdleTimerPackage;
+import com.rnfs.RNFSPackage;
+import com.dylanvann.fastimage.FastImageViewPackage;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
+import com.microsoft.codepush.react.CodePush;
+import com.learnta.clear.ClearCachePackage;
+import org.reactnative.camera.RNCameraPackage;
+import com.openinstall.openinstallLibrary.OpeninstallReactPackage;
+import com.reactnativecommunity.netinfo.NetInfoPackage;
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -15,6 +38,12 @@ import java.util.List;
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+
+        @Override
+        protected String getJSBundleFile() {
+        return CodePush.getJSBundleFile();
+        }
+    
     @Override
     public boolean getUseDeveloperSupport() {
       return BuildConfig.DEBUG;
@@ -24,6 +53,29 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFetchBlobPackage(),
+            new RNViewShotPackage(),
+            new ReactVideoPackage(),
+            new VectorIconsPackage(),
+            new SystemSettingPackage(),
+            new FPStaticServerPackage(),
+            new SplashScreenReactPackage(),
+            new PickerViewPackage(),
+            new OrientationPackage(),
+            new LocalBarcodeRecognizerPackage(),
+            new LinearGradientPackage(),
+            new ImagePickerPackage(),
+            new PickerPackage(),
+            new IdleTimerPackage(),
+            new RNFSPackage(),
+            new FastImageViewPackage(),
+            new RNDeviceInfo(),
+            new CodePush(getResources().getString(R.string.reactNativeCodePush_androidDeploymentKey), getApplicationContext(), BuildConfig.DEBUG),
+            new ClearCachePackage(),
+            new RNCameraPackage(),
+            new OpeninstallReactPackage(),
+            new NetInfoPackage(),
+            new AsyncStoragePackage(),
             new RNGestureHandlerPackage()
       );
     }
